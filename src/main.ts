329123fe0301d5c8b473b8b4fe16a7f9064fc974
Omar Franco <fofe2803@gmail.com>
2023-07-16 15:41:49 -0600

    feat: :sparkles: change the main file according to new module

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import { providerRouter } from "./providers/infrastructure/ProviderRouter";
-import path from "path";
+import { notificationRouter } from "./notifications/infrastructure/NotificationRouter";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/providers", providerRouter);
-app.get("/images-providers/:filename", (req, res) => {
-  const filename = req.params.filename;
-  const imagePath = path.join(__dirname, "images-providers", filename);
-  res.sendFile(imagePath);
-});
+app.use("/notificaions", notificationRouter);
 
 const port = parseInt(process.env.SERVER_PORT ?? "3000");
 
